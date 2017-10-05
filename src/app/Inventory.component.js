e7ce81c4d54132d4fa4e96efde6411add8827cc9
onestardev <faonestar514@gmail.com>
2017-10-05 04:34:33 +0200

    navigation

--- src/app/Inventory.component.js
+++ src/app/Inventory.component.js
@@ -5,17 +5,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
 var AppInventory = (function () {
-    function AppInventory() {
+    function AppInventory(_router) {
+        this._router = _router;
     }
+    AppInventory.prototype.onBack = function () {
+        this._router.navigate(['/Product']);
+    };
     return AppInventory;
 }());
 AppInventory = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: 'Inventory',
-    })
+        template: 'Inventory 
+            < a, class:  = "button"(click) = "onBack()" > Back, to: Products < /a>' 
+    }),
+    __metadata("design:paramtypes", [router_1.Router])
 ], AppInventory);
 exports.AppInventory = AppInventory;
 //# sourceMappingURL=Inventory.component.js.map
--- src/app/Inventory.component.ts
+++ src/app/Inventory.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { Router }  from '@angular/router';  
 
-@Component ({
-   selector: 'my-app',
-   template: 'Inventory',
-})
-export class AppInventory  {
+@Component ({  
+   selector: 'my-app',  
+   template: 'Inventory 
+   <a class = "button" (click) = "onBack()">Back to Products</a>' 
+})  
+
+export class AppInventory {  
+   constructor(private _router: Router){} 
+
+   onBack(): void { 
+      this._router.navigate(['/Product']); 
+   } 
 }
