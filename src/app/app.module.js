c5fb8b13ab278c9b7a39c57fabbe070bc069a00a
onestardev <faonestar514@gmail.com>
2017-10-10 18:11:51 +0200

    data display

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -8,7 +8,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var app_component_1 = require("./app.component");
-var ng2_pagination_1 = require("ng2-pagination");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -16,7 +15,7 @@ var AppModule = (function () {
 }());
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule, ng2_pagination_1.Ng2PaginationModule],
+        imports: [platform_browser_1.BrowserModule],
         declarations: [app_component_1.AppComponent],
         bootstrap: [app_component_1.AppComponent]
     })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
-import {Ng2PaginationModule} from 'ng2-pagination';
 
 @NgModule ({
-   imports:      [ BrowserModule,Ng2PaginationModule],
+   imports:      [ BrowserModule],
    declarations: [ AppComponent],
    bootstrap:    [ AppComponent ]
 })
